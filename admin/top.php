bba0b82494de64e58efe136d3766b51bbdd1dba6
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-02-18 22:11:33 +0530

    Logo and add to card button added

--- admin/top.php
+++ admin/top.php
@@ -1,3 +1,6 @@
+<?php
+ include '../env.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +10,9 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">   
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-    <title>Admin Side</title>
+    <title><?= $app_name ?> | Admin</title>
 </head>
 <body>
 <nav class="navbar navbar-dark bg-dark">
@@ -23,6 +27,9 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link  link-light" href="./brand.php">Brands</a>
+            </li>
+            <li class="nav-item">
+                  <a class="btn btn-outline-info" href="../">View Site</a>
             </li>
         </ul>
         
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  include 'env.php';
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,10 +10,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <title>Shopping App</title>
+    <title><?= $app_name ?></title>
     <style>
       .noResult{
         font-size:5vh;
@@ -22,7 +26,10 @@
 <body>
   <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand fs-4" href="#">Shopping App</a>
+       <div>
+          <img src="./favicon.png" style="object-fit: contain;height: 50px;width: 50px;">
+          <a class="navbar-brand fs-3 pt-2" href="./index.php"><?= $app_name ?></a>
+       </div>
         <div>
           <ul class="nav text-light  justify-content-end">
               <li class="nav-item">
@@ -37,9 +44,7 @@
               <li class="nav-item">
                   <a class="nav-link  link-light" href="#">Contact Us</a>
               </li>
-              <li class="nav-item">
-                  <a class="btn btn-outline-info" href="./admin/home.php">I'm Seller</a>
-              </li>
+              
           </ul>
         </div>
 
@@ -165,7 +170,7 @@
         <div class="d-flex justify-content-between mt-2">
           <p class="card-title"><?= $r['description'] ?></p>
           <div class="cost-button"> 
-            <a class="btn btn-primary" href="#" role="button">Buy</a>
+            <a class="btn btn-primary" href="#" role="button">Add To Cart</a>
           </div>                  
         </div>
       </div>
