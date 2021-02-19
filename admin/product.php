f89e0af13e0c191637c3def4c9efd5995e118aa0
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-02-19 23:42:30 +0530

    Product image update issue resolved

--- admin/product.php
+++ admin/product.php
@@ -3,8 +3,9 @@
  ?>
 
 <?php 
+error_reporting(0);
  $label = "Add";
-    $pname = $price = $stock = $c_id = $b_id = $desc = $file = "";
+    $pname = $price = $stock = $c_id = $b_id = $desc = $u_file = "";
     if (isset($_GET['update'])) {
         $label = "Update";
         $id = $_GET['update'];
@@ -17,7 +18,7 @@
         $c_id = $r['c_id'];
         $b_id = $r['b_id'];
         $desc = $r['description'];
-        $file = $r['p_img'];
+        $u_file = $r['p_img'];
     }
 ?>
 
@@ -87,9 +88,27 @@
             </div>
             </div>
             <div class="mb-3">
+            <?php 
+                if (isset($_GET['update'])){
+            ?>
+                <label for="formFile" class="form-label"> Change Product Image</label>
+                <div class="row">
+                    <div class="col-10">
+                        <input class="form-control" type="file" name="new_file" id="formFile">
+                    </div>
+                    <div class="col-2">
+                        <img src="<?= "../img/". $u_file ?>"  alt="product image" width="50p" height="40px">
+                    </div>                    
+                </div>
+            </div>
+            <?php 
+            }else{
+            ?>
                 <label for="formFile" class="form-label">Product Image</label>
                 <input class="form-control" type="file" name="file" id="formFile" value ="<?= $file ?>" required>
-            </div>
+            <?php
+            }
+            ?>
             <div class="col-6 m-auto align-item-center my-3 card">
                 <input class="btn btn-outline-primary" type="submit" name="add" value="<?= $label ?> Product">
             </div>
@@ -106,27 +125,32 @@
         $category = $_POST['category'];
         $brand = $_POST['brand'];
         $desc = $_POST['description'];
-        $file = $_FILES['file'];
-
-        $file_name = $file['name'];
-        $file_tmp = $file['tmp_name'];
-        $file_error = $file['error'];
-        $file_type = $file['type'];
-
-        $file_type = explode("/",$file_type);
-        $file_type = strtolower($file_type[0]);
-
-        if($file_type != "image"){
-            echo "<script>alert('Only Image file allowed.')</script>";
-            return;
+        if (isset($_GET['update'])) {
+            $file = $_FILES['new_file'];
         }else{
-            
-            if (!move_uploaded_file($file_tmp,"../img/".$file_name)) {
-                echo "<script>alert('Error while uploading file')</script>";
-                return;
-            }
+            $file = $_FILES['file'];
         }
-        
+        if ($_FILES['new_file']['name'] != null || $_FILES['file']['name'] != null) {   
+            $file_name = $file['name'];
+            $file_tmp = $file['tmp_name'];
+            $file_error = $file['error'];
+            $file_type = $file['type'];
+
+            $file_type = explode("/",$file_type);
+            $file_type = strtolower($file_type[0]);
+
+            if($file_type != "image"){
+                echo "<script>alert('Only Image file allowed.')</script>";
+                return;
+            }else{                
+                if (!move_uploaded_file($file_tmp,"../img/".$file_name)) {
+                    echo "<script>alert('Error while uploading file')</script>";
+                    return;
+                }
+            } 
+        }else{
+            $file_name = $u_file;
+        }
         $insert_query = "INSERT INTO product_details(p_name,description,price,stock,c_id,b_id,p_img) values('$pname','$desc',$price,$stock,$category,$brand,'$file_name')";
         
         $update_query = "UPDATE product_details SET p_name = '$pname', description = '$desc',price = $price, stock = $stock,c_id = $category , b_id= $brand, p_img = '$file_name'  where p_id = $id";
@@ -134,6 +158,7 @@
         if (isset($_GET['update'])) {
             if (mysqli_query($conn,$update_query)) {
                 echo "<script>alert('Product Update.')</script>";
+                //header("location:home.php");
              }else{
                  echo mysqli_error($conn);
              }
@@ -141,11 +166,9 @@
             if (mysqli_query($conn,$insert_query)) {
                 echo "<script>alert('Product Added.')</script>";
              }else{
-                 echo mysqli_error($conn);
+                echo mysqli_error($conn);
              }
         }
-
-
     }
 ?>
 <?php include 'bottom.php' ?>
--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     <div class=" col-md-3 col-sm-12 p-3 col-xl-4 text-center mt-3">
 
       <div class="form-outline d-flex">
-        <input type="text" id="searchKey" name="searchKey" class="form-control" placeholder="Search.." list="searchResult"/>
+        <input type="text" id="searchKey" name="searchKey" class="form-control" placeholder="Search.." list="searchResult" autocomplete="off"/>
         <button type="submit" name="btnSearch" class="btn btn-outline-success px-4">
             <i class="fa fa-search" aria-hidden="true"></i>
         </button>      
