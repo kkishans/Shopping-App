2452d6913aad8cef75bcb3cd57866b4552f113d8
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-02-25 22:59:21 +0530

    Database file updated

--- db/shopping.sql
+++ db/shopping.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2021 at 12:46 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Feb 25, 2021 at 06:28 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,160 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin_details`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `admin_details` (
-  `aid` int(10) NOT NULL,
-  `fname` varchar(255) NOT NULL,
-  `lname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+CREATE TABLE `users` (
+  `u_id` int(10) NOT NULL,
+  `f_name` text NOT NULL,
+  `l_name` text NOT NULL,
+  `email` text NOT NULL,
+  `phone` text NOT NULL,
+  `address` text NOT NULL,
+  `pass` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `admin_details`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `admin_details` (`aid`, `fname`, `lname`, `email`, `password`) VALUES
-(1, 'Nikunj', 'Thakor', 'nikunj@gmail.com', '1234');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `brands`
---
-
-CREATE TABLE `brands` (
-  `b_id` int(10) NOT NULL,
-  `b_name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `brands`
---
-
-INSERT INTO `brands` (`b_id`, `b_name`) VALUES
-(1, 'Lenevo'),
-(3, 'Apple'),
-(4, 'Dell');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `c_id` int(10) NOT NULL,
-  `c_title` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`c_id`, `c_title`) VALUES
-(1, 'Electronics '),
-(3, 'Music & Video Player');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_details`
---
-
-CREATE TABLE `product_details` (
-  `p_id` int(10) NOT NULL,
-  `p_name` text NOT NULL,
-  `description` text NOT NULL,
-  `p_img` text NOT NULL,
-  `price` double NOT NULL,
-  `stock` int(30) NOT NULL,
-  `c_id` int(11) NOT NULL,
-  `b_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `product_details`
---
-
-INSERT INTO `product_details` (`p_id`, `p_name`, `description`, `p_img`, `price`, `stock`, `c_id`, `b_id`) VALUES
-(7, 'Smart watch', 'Smart watch', 'IMG_20210214_133608.jpg', 20000, 2, 1, 3),
-(8, 'Desktop Monitor', 'Wide monitor', 'u_10181535.jpg', 15000, 12, 1, 3),
-(9, 'Laptop 2', 'none', 'img1.jpg', 100000, 1, 1, 4),
-(10, 'Tablet', 'none', 'MINIDDHQIBFYW-medium.jpg', 120000, 5, 1, 3),
-(11, 'Lenovo ideapad 3', 'Performence Beast', 'lenovo_ideapad_3.png', 70000, 2, 1, 1),
-(12, 'Lenovo legion', 'Performence Beast', 'legion.png', 65000, 1, 1, 1);
+INSERT INTO `users` (`u_id`, `f_name`, `l_name`, `email`, `phone`, `address`, `pass`) VALUES
+(6, 'Kishan', 'Khant', 'kishan@mail.com', '07096897145', 'Bharuch', '81dc9bdb52d04dc20036dbd8313ed055');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin_details`
+-- Indexes for table `users`
 --
-ALTER TABLE `admin_details`
-  ADD PRIMARY KEY (`aid`);
-
---
--- Indexes for table `brands`
---
-ALTER TABLE `brands`
-  ADD PRIMARY KEY (`b_id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`c_id`);
-
---
--- Indexes for table `product_details`
---
-ALTER TABLE `product_details`
-  ADD PRIMARY KEY (`p_id`),
-  ADD KEY `c_id` (`c_id`,`b_id`),
-  ADD KEY `b_id` (`b_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin_details`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `admin_details`
-  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `brands`
---
-ALTER TABLE `brands`
-  MODIFY `b_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `c_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `product_details`
---
-ALTER TABLE `product_details`
-  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `product_details`
---
-ALTER TABLE `product_details`
-  ADD CONSTRAINT `product_details_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `brands` (`b_id`),
-  ADD CONSTRAINT `product_details_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `category` (`c_id`);
+ALTER TABLE `users`
+  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
