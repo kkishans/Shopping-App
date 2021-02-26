218299da9e872cbfb86281281f1818f710e75821
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-02-27 00:16:10 +0530

    Database Updated

--- db/shopping.sql
+++ db/shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2021 at 06:28 PM
+-- Generation Time: Feb 26, 2021 at 07:45 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -20,6 +20,144 @@ SET time_zone = "+00:00";
 --
 -- Database: `shopping`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_details`
+--
+
+CREATE TABLE `admin_details` (
+  `aid` int(10) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_details`
+--
+
+INSERT INTO `admin_details` (`aid`, `fname`, `lname`, `email`, `pass`) VALUES
+(1, 'Nikunj', 'Thakor', 'nikunj@gmail.com', ' 1234'),
+(2, 'Kishan', 'Khant', 'kishan@mail.com', ' 1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `b_id` int(10) NOT NULL,
+  `b_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`b_id`, `b_name`) VALUES
+(1, 'Lenevo'),
+(3, 'Apple'),
+(4, 'Dell');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `c_id` int(10) NOT NULL,
+  `c_title` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`c_id`, `c_title`) VALUES
+(1, 'Electronics '),
+(3, 'Music & Video Player');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ordered_products`
+--
+
+CREATE TABLE `ordered_products` (
+  `id` int(11) NOT NULL,
+  `o_id` int(8) NOT NULL,
+  `p_id` int(8) NOT NULL,
+  `status` text NOT NULL DEFAULT 'Not Delivered'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ordered_products`
+--
+
+INSERT INTO `ordered_products` (`id`, `o_id`, `p_id`, `status`) VALUES
+(1, 1, 8, 'Not Delivered'),
+(2, 1, 7, 'Not Delivered'),
+(3, 2, 10, 'Not Delivered'),
+(4, 3, 7, 'Not Delivered'),
+(5, 4, 12, 'Not Delivered');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `o_id` int(8) NOT NULL,
+  `u_id` int(8) NOT NULL,
+  `total_amount` double NOT NULL,
+  `shipping_address` varchar(300) NOT NULL,
+  `ordered_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`o_id`, `u_id`, `total_amount`, `shipping_address`, `ordered_at`) VALUES
+(1, 6, 35000, 'Bharuch', '2021-02-26 17:36:26'),
+(2, 6, 120000, 'Bharuch', '2021-02-26 17:38:56'),
+(3, 6, 20000, 'Bharuch', '2021-02-26 17:40:19'),
+(4, 6, 65000, 'Bharuch', '2021-02-26 18:43:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_details`
+--
+
+CREATE TABLE `product_details` (
+  `p_id` int(10) NOT NULL,
+  `p_name` text NOT NULL,
+  `description` text NOT NULL,
+  `p_img` text NOT NULL,
+  `price` double NOT NULL,
+  `stock` int(30) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `b_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_details`
+--
+
+INSERT INTO `product_details` (`p_id`, `p_name`, `description`, `p_img`, `price`, `stock`, `c_id`, `b_id`) VALUES
+(7, 'Smart watch', 'Smart watch', 'IMG_20210214_133608.jpg', 20000, 2, 1, 3),
+(8, 'Desktop Monitor', 'Wide monitor', 'u_10181535.jpg', 15000, 12, 1, 3),
+(9, 'Laptop 2', 'none', 'img1.jpg', 100000, 1, 1, 4),
+(10, 'Tablet', 'none', 'MINIDDHQIBFYW-medium.jpg', 120000, 5, 1, 3),
+(11, 'Lenovo ideapad 3', 'Performence Beast', 'lenovo_ideapad_3.png', 70000, 2, 1, 1),
+(12, 'Lenovo legion', 'Performence Beast', 'legion.png', 65000, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -49,6 +187,44 @@ INSERT INTO `users` (`u_id`, `f_name`, `l_name`, `email`, `phone`, `address`, `p
 --
 
 --
+-- Indexes for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`b_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `ordered_products`
+--
+ALTER TABLE `ordered_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`o_id`);
+
+--
+-- Indexes for table `product_details`
+--
+ALTER TABLE `product_details`
+  ADD PRIMARY KEY (`p_id`),
+  ADD KEY `c_id` (`c_id`,`b_id`),
+  ADD KEY `b_id` (`b_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -59,10 +235,57 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_details`
+--
+ALTER TABLE `admin_details`
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `b_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `c_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ordered_products`
+--
+ALTER TABLE `ordered_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `o_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product_details`
+--
+ALTER TABLE `product_details`
+  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product_details`
+--
+ALTER TABLE `product_details`
+  ADD CONSTRAINT `product_details_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `brands` (`b_id`),
+  ADD CONSTRAINT `product_details_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `category` (`c_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
