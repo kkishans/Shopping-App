356a32dd9510e409dbcad0d313d12122f63f487c
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-03-05 22:36:24 +0530

    Orders sorting by date

--- admin/orders.php
+++ admin/orders.php
@@ -8,7 +8,29 @@
 <div>
     <h1 class="text-center my-3"> All Orders</h1>
 </div>
-    
+
+<div class="align-items-center text-center card border-0">
+<form action="#" method="post">
+<div class="d-flex just-content-center row">
+        <div class="col-md-6 col-xl-6 col-sm-3 m-3">
+        <?php
+            if (isset($_POST['filterdate'])) {
+               $date = $_POST['filterdate'];
+            }else{
+                $date = date('d-m-Y');
+             }
+        ?>
+            <input type="date"  id="datepicker"  name="filterdate" class="form-control col-md-6" value="<?= date('Y-m-d')?>">
+
+        </div>
+        <div class="col-md-5 col-xl-3 col-sm-3 m-3">
+            <input type="submit" name="submit" class="btn btn-success" value="Filter">
+        </div>
+        
+    </div>
+</form>
+</div>
+
 <div class="card col-md-10 col-xl-10 col-sm-11 m-auto" >
   <div class="card-body">
     <table class="table table-striped" >
@@ -18,32 +40,23 @@
                 <th>User Name</th>
                 <th>Product Name</th>
                 <th>Order At</th>
-                <th>Delivery address </th>
                 <th>Status</th>
             </tr>
         </thead>
         <tbody>
         <?php 
-         $query = "SELECT * FROM ordered_products";
-         $res = mysqli_query($conn,$query);
+         
+         $order_query = "SELECT O.o_id o_id, f_name,l_name, p_name , ordered_at , status FROM ordered_products AS OP, order_details AS O, users AS U, product_details  AS P WHERE  O.o_id = OP.o_id AND O.u_id = U.u_id AND OP.o_id = O.o_id AND P.p_id = OP.p_id AND DATE_FORMAT(ordered_at, '%Y-%m-%d') =  '$date'";
+         $res = mysqli_query($conn,$order_query);
           if (mysqli_num_rows($res) > 0) {
-             while($r = mysqli_fetch_assoc($res)){
-                $order_query = "SELECT * FROM order_details WHERE o_id = ".$r['o_id'] ;
-                $res1 = mysqli_query($conn,$order_query);
-                $o = mysqli_fetch_assoc($res1);
-                $user_query = "SELECT * FROM users WHERE u_id = ".$o['u_id'] ;
-                $res2 = mysqli_query($conn,$user_query);
-                $u = mysqli_fetch_assoc($res2);
-                $product_query = "SELECT * FROM product_details WHERE p_id = ".$r['p_id'] ;
-                $res3 = mysqli_query($conn,$product_query);
-                $p = mysqli_fetch_assoc($res3);
-        ?>
+             while($r = mysqli_fetch_assoc($res)){           
+
+                ?>
             <tr>
-                <th><?= $r['id'] ?></th>
-                <th><?= $u['f_name']." ".$u['l_name'] ?> </th>
-                <th><?= $p['p_name'] ?></th>
-                <th><?= $o['ordered_at'] ?></th>
-                <th><?= $o['shipping_address'] ?></th>
+                <th><?= $r['o_id'] ?></th>
+                <th><?= $r['f_name']." ".$r['l_name'] ?> </th>
+                <th><?= $r['p_name'] ?></th>
+                <th><?= $r['ordered_at'] ?></th>
                 <?php if($r['status'] == "Not Delivered") {   ?>
                 <th><a href="./delete.php?productDelivered=<?= $r['o_id'] ?>&productId=<?= $r['p_id'] ?>" class="btn btn-success">Not Delivered</a></th>
                 <?php  }else{ ?>
@@ -55,7 +68,7 @@
              }
             }else{
                 echo "
-                    <tr><p' align='center'> No Data Found.</p></tr>
+                    <tr><p' align='center'> No Data Found On This Date</p></tr>
                 ";
             }    
             echo mysqli_error($conn)   ;     
