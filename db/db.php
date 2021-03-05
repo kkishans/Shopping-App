4b28b53a019edf544f7b50c5f29ef762ff3a4224
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-03-05 23:33:34 +0530

    Create database if not exist

--- db/db.php
+++ db/db.php
@@ -4,15 +4,13 @@
     $pass="";
     $db_name="shopping";
 
-    // $conn = mysqli_connect($host,$user,$pass);
+    $conn = mysqli_connect($host,$user,$pass);
 
-    // if (!mysqli_query($conn,$create_database)) {
-    //     echo "<script>alert('Error while creating databse.') ". mysqli_error($conn)."</script>";
-    // }
+    if (!$conn) {
+        echo "<script>alert('Error while creating databse.') </script>";
+    }
    
-    // $create_database = "CREATE DATABASE IF NOT EXISTS  $db_name ";
-   
-    // echo mysqli_error($conn);
+    $create_database = "CREATE DATABASE IF NOT EXISTS  $db_name ";  
     $conn = mysqli_connect($host,$user,$pass,$db_name);
     
     if(!$conn){
