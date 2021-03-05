7215bffbb58a2f7ab62e8acf93e672df1d0232ed
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-03-06 00:24:05 +0530

    Category wise filter added on home page of admin

--- admin/home.php
+++ admin/home.php
@@ -5,41 +5,46 @@
         header("Location: login.php");
     }
 ?>
-<div class="col-xl-5 col-md-5 col-sm-10 col-xm-11">
-        <h1 class="text-center my-3"> Our Collection</h1>
-    </div>
-    <div class="col-xl-7 col-md-7 col-sm-10 col-xm-11">
-    <form action="#"  class="row">
-    <div class="col-xl-3 col-md-3 col-sm-5 ">
-      <label class="form-label">Category :</label>
-        <select class="form-select" name="category" >
-          <option value="0">All</option>
-          <?php 
-          
-            $query = "select * from category";
-
-            $result = mysqli_query($conn,$query);
-            $c_id = isset($_GET["category"]) ? $_GET["category"] : 0; 
-            if (mysqli_num_rows($result) > 0) {
-                while($r = mysqli_fetch_assoc($result)){
-                  print_r($r);
-                  $s = ($c_id == $r["c_id"]) ? "selected" : "";
-                    echo "<option value='". $r["c_id"]."' $s > ".$r["c_title"]."</option>";
-                }
-            }
-            else echo "0 record in category";
-            ?>
-        </select> 
-    </div>
-    <div class=" col-md-1 col-sm-5 p-3 col-xl-2 text-center mt-3 ">
-        <button type="submit"  name="filter" class="btn btn-outline-success px-4">Filter</button>
-    </div>
-    </form>
-    </div>
 <div>
     <h1 class="text-center my-3"> All Products</h1>
 </div>
-<div class="card col-11 m-auto" >
+<div class="align-items-center text-center card border-0">
+    <form action="#" method="post">
+        <div class="d-flex just-content-center row">
+            <div class=" col-md-2 col-sm-3  col-xl-3 mt-3 ">
+                <label class="form-label">Category :</label>
+            </div>
+            <div class=" col-md-5 col-sm-5  col-xl-5 mt-3 ">           
+               
+                <select class="form-select" name="category" >
+                <option value="0">All</option>
+                <?php 
+                
+                    $query = "select * from category";
+
+                    $result = mysqli_query($conn,$query);
+                    $c_id = isset($_POST["category"]) ? $_POST["category"] : 0; 
+                    if (mysqli_num_rows($result) > 0) {
+                        while($r = mysqli_fetch_assoc($result)){
+                        print_r($r);
+                        $s = ($c_id == $r["c_id"]) ? "selected" : "";
+                            echo "<option value='". $r["c_id"]."' $s > ".$r["c_title"]."</option>";
+                        }
+                    }
+                    else echo "0 record in category";
+                    ?>
+                </select> 
+            </div>
+            <div class=" col-md-5 col-sm-4 col-xl-4 mt-3 ">
+                <button type="submit"  name="filter" class="btn btn-outline-success px-4">Filter</button>
+            </div>
+        </div>
+       
+        </div>
+    </form>
+</div>
+
+<div class="card col-11 m-auto mt-5" >
   <div class="card-body">
     <table class="table table-striped" >
         <thead>
@@ -56,7 +61,10 @@
         <?php 
           include '../db/db.php';
           $query = "select * from product_details";
-
+          if (isset($_POST['category'])) {
+              if( $_POST['category'] != '0')
+                $query .= " where c_id = ".$_POST['category'];
+          }
           $result = mysqli_query($conn,$query);
 
           if (mysqli_num_rows($result) > 0) {
