2fa38455796734a43eabbd5c7815b1d94d5669b1
Kishan Khant <38679780+kkishans@users.noreply.github.com>
2021-03-05 23:16:00 +0530

    improve design of cart page

--- user/cart.php
+++ user/cart.php
@@ -28,117 +28,120 @@
         <div class="col-md-4 ms-4">
             <h3>Your Cart</h3>
         </div>
-        <?php
-            
-        ?>
-            <div class="col-md-3 text-end me-4">
+        <div class="col-md-3 text-end me-4">
             <form  method="post">
                 <input type="submit" class="btn btn-outline-danger" value="Remove all" name="clear">
             </form>
         </div>
-        <?php
-            
-        ?>
-        
-        
     </div>
     <hr>
-    
-    <?php
-        
-    ?>
 
     <div class="mt-4">
-            <div class="card col-11 m-auto" >
-                <div class="card-body">
-                    <table class="table table-striped text-center" >
-                        <thead>
-                            <tr>
-                                <th>Review</th>
-                                <th>Product Name</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
-                                <th>Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php 
-                            $total = 0;
-                            if(isset($_SESSION['useremail'])){
-                            $sql = "SELECT P.p_id,id,p_name,qty,p_img,price from cart_details as C,product_details as P,users as U where  C.p_id = P.p_id and U.u_id = C.u_id and is_in_cart = 'y' and U.email = '".$_SESSION['useremail']."'";
-                            
-                            $res = mysqli_query($conn,$sql);
-                            if (mysqli_num_rows($res) > 0) {
-                            
-                            
-                                while( $v = mysqli_fetch_assoc($res)){
-                                    $totalPerProduct = $v['price'] * $v['qty'];
-                                    $total += $totalPerProduct;
-
-                        ?>
-
-                            <tr onclick="window.location=' ../product.php?id=<?= $v['p_id']  ?>'" style="cursor:pointer">
-                            
-                                
-                                <td><img src="<?= "../img/". $v['p_img']  ?>" alt="product image" width="60px" height="60px"></td>
-                                <th><?= $v['p_name'] ?></th>
-                                <th><?= $v['price'] ?></th>
-                                <th><?= $v['qty'] ?></th> 
-                                <th><a href="./remove_product.php?index=<?= $v['id'] ?>" class="btn btn-outline-danger"> <i class="fa fa-remove" aria-hidden="true"></i> </a></th>
-                                
-                            </tr>
-                           
-                            <?php 
-
-                                }
-                            }
-                            else{
+        <div class="card col-11 m-auto" >
+            <div class="card-body">
+            <?php 
+                $total = 0;
+                if(isset($_SESSION['useremail'])){
+                $sql = "SELECT P.p_id,id,p_name,qty,p_img,price from cart_details as C,product_details as P,users as U where  C.p_id = P.p_id and U.u_id = C.u_id and is_in_cart = 'y' and U.email = '".$_SESSION['useremail']."'";
+                
+                $res = mysqli_query($conn,$sql);
+                if (mysqli_num_rows($res) > 0) {
                             ?>
-                                <div class="col-4 m-auto" >
-                                        <img src="../img/emptyCart" alt="Cart is empty" height="300px" srcset="">   
-                                        <h4 class="text-center">Your Cart is Empty!!!</h4> 
-                                </div>
-                            <?php 
+                <table class="table table-striped text-center" >
+                    <thead>
+                        <tr>
+                            <th>Review</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php                             
+                        while( $v = mysqli_fetch_assoc($res)){
+                        $totalPerProduct = $v['price'] * $v['qty'];
+                        $total += $totalPerProduct;
+                    ?>
+
+                        <tr onclick="window.location=' ../product.php?id=<?= $v['p_id']  ?>'" style="cursor:pointer">
+                        
+                            
+                            <td><img src="<?= "../img/". $v['p_img']  ?>" alt="product image" width="60px" height="60px"></td>
+                            <th><?= $v['p_name'] ?></th>
+                            <th><?= $v['price'] ?></th>
+                            <th><?= $v['qty'] ?></th> 
+                            <th><a href="./remove_product.php?index=<?= $v['id'] ?>" class="btn btn-outline-danger"> <i class="fa fa-remove" aria-hidden="true"></i> </a></th>
+                            
+                        </tr>
+                        
+                        <?php 
+
                             }
                         }
                         else{
-
-                            $total = 0;
-                            if(isset($_SESSION['cart'])){
-                                foreach($_SESSION['cart'] as $k => $v){
-                                    $totalPerProduct = $v['price'] * $v['qty'];
-                                    $total += $totalPerProduct;
-
                         ?>
-                            <tr onclick="window.location=' ../product.php?id=<?= $v['id']  ?>'" style="cursor:pointer">
-                                <td><img src="<?= "../img/". $v['p_img']  ?>" alt="product image" width="60px" height="60px"></td>
-                                <th><?= $v['p_name'] ?></th>
-                                <th><?= $v['price'] ?></th>
-                                <th><?= $v['qty'] ?></th> 
-                                <th><a href="./remove_product.php?index=<?= $k ?>" class="btn btn-outline-danger"> <i class="fa fa-remove" aria-hidden="true"></i> </a></th>
-                            </tr>
-                            <?php 
-
-                                }
-                            }
-                            
+                            <div class="col-4 m-auto" >
+                                    <img src="../img/emptyCart" alt="Cart is empty" height="300px" srcset="">   
+                                    <h4 class="text-center">Your Cart is Empty!!!</h4> 
+                            </div>
+                        <?php 
                         }
-                        ?>
+                    }
+                    else{
 
-                            
-                        </tbody>
-                    </table>
-                    <div class="mt-3">
+                        $total = 0;
+                        if(isset($_SESSION['cart'])){
+                            foreach($_SESSION['cart'] as $k => $v){
+                                $totalPerProduct = $v['price'] * $v['qty'];
+                                $total += $totalPerProduct;
+
+                    ?>
+                        <tr onclick="window.location=' ../product.php?id=<?= $v['id']  ?>'" style="cursor:pointer">
+                            <td><img src="<?= "../img/". $v['p_img']  ?>" alt="product image" width="60px" height="60px"></td>
+                            <th><?= $v['p_name'] ?></th>
+                            <th><?= $v['price'] ?></th>
+                            <th><?= $v['qty'] ?></th> 
+                            <th><a href="./remove_product.php?index=<?= $k ?>" class="btn btn-outline-danger"> <i class="fa fa-remove" aria-hidden="true"></i> </a></th>
+                        </tr>
+                        <?php 
+
+                            }
+                        }
+                        
+                    }
+                    ?>
+
+                        
+                    </tbody>
+                </table>
+                <?php
+                    if(mysqli_num_rows($res) > 0 || isset($_SESSION['cart']) ){
+                        ?>
+                        <div class="mt-3">
                             <h5 class="text-end pe-2">Total : <?= $total?></h5>
-                    </div>
-                </div>
+                        </div>
+                        
+                        <?php
+                    }
+                
+                ?>
+            </div>
         </div>
     </div>
-    <div class="justify-content-center d-flex">
-    <form action="" method="post">
-        <button type="submit" name="order" class="btn btn-outline-primary my-5 m-auto fs-4">Order Now</button>
-    </form>
-    </div>
+    <?php
+        if(mysqli_num_rows($res) > 0 || isset($_SESSION['cart']) ){
+            ?>
+            <div class="justify-content-center d-flex">
+                <form action="" method="post">
+                    <button type="submit" name="order" class="btn btn-outline-primary my-5 m-auto fs-4">Order Now</button>
+                </form>
+            </div>
+            <?php
+        }
+    
+    ?>
+   
     <?php
         
     ?>
