ea7a69c3e6f0f51354f41e2e21bd7a5213f496df
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2021-03-06 12:42:29 +0530

    db.php queries add with constraints

--- db/db.php
+++ db/db.php
@@ -7,14 +7,150 @@
     $conn = mysqli_connect($host,$user,$pass);
 
     if (!$conn) {
-        echo "<script>alert('Error while creating databse.') </script>";
+        echo "<script>alert('Error while creating database.') </script>";
     }
    
     $create_database = "CREATE DATABASE IF NOT EXISTS  $db_name ";  
-    $conn = mysqli_connect($host,$user,$pass,$db_name);
+    $res = mysqli_query($conn,$create_database);
+    if ($res) {
+        $conn = mysqli_connect($host,$user,$pass,$db_name);
+        
+    }
+
+    $create_admin_details = "CREATE TABLE IF NOT EXISTS `admin_details` (
+        `aid` int(10) primary key,
+        `fname` varchar(255) NOT NULL,
+        `lname` varchar(255) NOT NULL,
+        `email` varchar(255) NOT NULL,
+        `password` varchar(255) NOT NULL
+      )";
+    $res = mysqli_query($conn,$create_admin_details);
+
+    $create_brands = "CREATE TABLE IF NOT EXISTS `brands` (
+        `b_id` int(10) primary key,
+        `b_name` text NOT NULL
+      )";
+    $res = mysqli_query($conn,$create_brands);
+
+    $create_cart_deatils = "CREATE TABLE IF NOT EXISTS `cart_details` (
+        `id` int(10) primary key,
+        `p_id` int(10) NOT NULL,
+        `u_id` int(10) NOT NULL,
+        `qty` int(10) NOT NULL,
+        `is_in_cart` varchar(15) NOT NULL
+      )";
+    $res = mysqli_query($conn,$create_cart_deatils);
+
+    $create_category = "CREATE TABLE IF NOT EXISTS `category` (
+        `c_id` int(10) primary key,
+        `c_title` text NOT NULL
+      )";
+    $res = mysqli_query($conn,$create_category);
+
+    $create_ordered_product = "CREATE TABLE IF NOT EXISTS `ordered_products` (
+        `id` int(11) primary key,
+        `o_id` int(8) NOT NULL,
+        `p_id` int(8) NOT NULL,
+        `status` text NOT NULL DEFAULT 'Not Delivered'
+      )";
+    $res = mysqli_query($conn,$create_ordered_product);
     
+    $create_order_details = "CREATE TABLE IF NOT EXISTS `order_details` (
+        `o_id` int(8) primary key,
+        `u_id` int(8) NOT NULL,
+        `total_amount` double NOT NULL,
+        `shipping_address` varchar(300) NOT NULL,
+        `ordered_at` timestamp NOT NULL DEFAULT current_timestamp()
+      )";
+    $res = mysqli_query($conn,$create_order_details);
+    
+    $craete_product_details = "CREATE TABLE IF NOT EXISTS `product_details` (
+        `p_id` int(10) primary key,
+        `p_name` text NOT NULL,
+        `description` text NOT NULL,
+        `p_img` text NOT NULL,
+        `price` double NOT NULL,
+        `stock` int(30) NOT NULL,
+        `c_id` int(11) NOT NULL,
+        `b_id` int(11) NOT NULL
+      )";
+
+    $res = mysqli_query($conn,$craete_product_details);
+
+    $create_users = "CREATE TABLE IF NOT EXISTS `users` (
+        `u_id` int(10)  primary key,
+        `f_name` text NOT NULL,
+        `l_name` text NOT NULL,
+        `email` text NOT NULL,
+        `phone` text NOT NULL,
+        `address` text NOT NULL,
+        `pass` text NOT NULL
+      )";
+
+    $res = mysqli_query($conn,$create_users);
+
+    //Relationship between tables
+
+    $alter = "ALTER TABLE `admin_details`
+    MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;";
+    $res = mysqli_query($conn,$alter);
+    $alter = "ALTER TABLE `brands`
+        MODIFY `b_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `cart_details`
+        MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `category`
+        MODIFY `c_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `ordered_products`
+        MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `order_details`
+        MODIFY `o_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `product_details`
+        MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;";
+    
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `users`
+        MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;";
+        $res = mysqli_query($conn,$alter);
+        
+        $alter = "ALTER TABLE `product_details`
+        ADD CONSTRAINT `product_details_ibfk_1` FOREIGN KEY (`b_id`) REFERENCES `brands` (`b_id`),
+        ADD CONSTRAINT `product_details_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `category` (`c_id`);";
+
+        $res = mysqli_query($conn,$alter);
+
+        $alter = "ALTER TABLE `cart_details`
+        ADD CONSTRAINT `cart_details_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`),
+        ADD CONSTRAINT `cart_details_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product_details` (`p_id`);";
+
+        $res = mysqli_query($conn,$alter);
+
+        $alter = "ALTER TABLE `ordered_products`
+          ADD CONSTRAINT `ordered_products_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product_details` (`p_id`);";
+
+        $res = mysqli_query($conn,$alter);
+        $alter = "ALTER TABLE `order_details`
+          ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);";
+        $res = mysqli_query($conn,$alter);
+
+
     if(!$conn){
-        echo "<script>alert('Error while connecting to databse.')</script>";
+        echo "<script>alert('Error while connecting to database.')</script>";
         return;
     }
+
+
+    
+    
+
+
 ?>
--- db/shopping.sql
+++ db/shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2021 at 03:39 PM
+-- Generation Time: Mar 06, 2021 at 08:11 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -40,10 +40,7 @@ CREATE TABLE `admin_details` (
 --
 
 INSERT INTO `admin_details` (`aid`, `fname`, `lname`, `email`, `password`) VALUES
-(3, 'test', 'test', 'test@mail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-(4, 'Nikunj', 'Thakor', 'nikunj@mail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-(5, 'Kishan', 'Khant', 'kishan@mail.com', '81dc9bdb52d04dc20036dbd8313ed055'),
-(6, 'nn', 'nn', 'nn@mail.com', '123');
+(9, 'Nikunj', 'Thakor', 'thakornikunj152@gmail.com', '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -61,10 +58,8 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`b_id`, `b_name`) VALUES
-(1, 'Lenevo'),
-(3, 'Apple'),
-(4, 'Dell'),
-(8, 'Acer');
+(9, 'Acer'),
+(10, 'Lenovo');
 
 -- --------------------------------------------------------
 
@@ -85,7 +80,11 @@ CREATE TABLE `cart_details` (
 --
 
 INSERT INTO `cart_details` (`id`, `p_id`, `u_id`, `qty`, `is_in_cart`) VALUES
-(9, 7, 9, 2, 'y');
+(11, 13, 10, 1, 'n'),
+(12, 13, 10, 1, 'n'),
+(13, 13, 10, 1, 'n'),
+(14, 13, 10, 1, 'n'),
+(15, 13, 10, 1, 'n');
 
 -- --------------------------------------------------------
 
@@ -103,10 +102,7 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`c_id`, `c_title`) VALUES
-(1, 'Electronics'),
-(3, 'Music & Video Player'),
-(4, 'Laptop'),
-(6, 'TV');
+(7, 'Laptop');
 
 -- --------------------------------------------------------
 
@@ -126,17 +122,8 @@ CREATE TABLE `ordered_products` (
 --
 
 INSERT INTO `ordered_products` (`id`, `o_id`, `p_id`, `status`) VALUES
-(1, 1, 8, 'Delivered'),
-(2, 1, 7, 'Not Delivered'),
-(3, 2, 10, 'Delivered'),
-(4, 3, 7, 'Not Delivered'),
-(5, 4, 12, 'Not Delivered'),
-(6, 7, 7, 'Not Delivered'),
-(7, 8, 7, 'Not Delivered'),
-(8, 8, 11, 'Not Delivered'),
-(9, 9, 7, 'Not Delivered'),
-(10, 9, 9, 'Not Delivered'),
-(11, 9, 12, 'Not Delivered');
+(12, 5, 13, 'Not Delivered'),
+(13, 6, 13, 'Not Delivered');
 
 -- --------------------------------------------------------
 
@@ -157,16 +144,12 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`o_id`, `u_id`, `total_amount`, `shipping_address`, `ordered_at`) VALUES
-(1, 6, 35000, 'Bharuch', '2021-02-26 17:36:26'),
-(2, 6, 120000, 'Bharuch', '2021-02-26 17:38:56'),
-(3, 6, 20000, 'Bharuch', '2021-02-26 17:40:19'),
-(4, 6, 65000, 'Bharuch', '2021-02-26 18:43:27'),
-(5, 7, 35000, 'Bharuch', '2021-02-27 07:12:39'),
-(6, 7, 35000, 'Bharuch', '2021-02-27 07:13:19'),
-(7, 7, 20000, 'Bharuch', '2021-02-27 12:04:53'),
-(8, 7, 90000, 'Bharuch', '2021-02-27 12:05:19'),
-(9, 7, 225000, 'Bharuch', '2021-02-27 13:58:21'),
-(10, 7, 15000, 'Bharuch', '2021-03-03 13:01:57');
+(1, 10, 12000, 'BHaruch', '2021-03-06 07:01:05'),
+(2, 10, 0, 'BHaruch', '2021-03-06 07:01:11'),
+(3, 10, 12000, 'BHaruch', '2021-03-06 07:01:45'),
+(4, 10, 12000, 'BHaruch', '2021-03-06 07:04:52'),
+(5, 10, 12000, 'BHaruch', '2021-03-06 07:08:09'),
+(6, 10, 12000, 'BHaruch', '2021-03-06 07:08:53');
 
 -- --------------------------------------------------------
 
@@ -190,12 +173,7 @@ CREATE TABLE `product_details` (
 --
 
 INSERT INTO `product_details` (`p_id`, `p_name`, `description`, `p_img`, `price`, `stock`, `c_id`, `b_id`) VALUES
-(7, 'Smart watch', 'Smart watch', 'IMG_20210214_133608.jpg', 20000, 2, 1, 3),
-(8, 'Desktop Monitor', 'Wide monitor', 'u_10181535.jpg', 15000, 12, 1, 3),
-(9, 'Laptop 2', 'none', 'img1.jpg', 100000, 1, 1, 4),
-(10, 'Tablet', 'none', 'MINIDDHQIBFYW-medium.jpg', 120000, 5, 1, 3),
-(11, 'Lenovo ideapad 3', 'Performence Beast', 'lenovo_ideapad_3.png', 70000, 2, 1, 1),
-(12, 'Lenovo legion', 'Performence Beast', 'legion.png', 65000, 1, 1, 1);
+(13, 'Lenovo ideapad 3', 'Performence Beast', 'lenovo_ideapad_3.png', 12000, 1, 7, 10);
 
 -- --------------------------------------------------------
 
@@ -218,10 +196,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `f_name`, `l_name`, `email`, `phone`, `address`, `pass`) VALUES
-(6, 'Kishan', 'Khant', 'kishan@mail.com', '07096897145', 'Bharuch', '81dc9bdb52d04dc20036dbd8313ed055'),
-(7, 'Nikunj', 'Thakor', 'nikunj@mail.com', '9876543210', 'Bharuch', '202cb962ac59075b964b07152d234b70'),
-(8, 'Test', 'test', 'test1@gmail.com', '9865327410', 'bharuch', '202cb962ac59075b964b07152d234b70'),
-(9, 'kk', 'kk', 'kk@mail.com', '7894561230', 'bharuch', '202cb962ac59075b964b07152d234b70');
+(10, 'Nikunj', 'Thakor', 'thakornikunj152@gmail.com', '8980112582', 'BHaruch', '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
@@ -244,7 +219,8 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `cart_details`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `p_id` (`p_id`,`u_id`);
+  ADD KEY `cart_details_ibfk_1` (`u_id`),
+  ADD KEY `cart_details_ibfk_2` (`p_id`);
 
 --
 -- Indexes for table `category`
@@ -256,28 +232,30 @@ ALTER TABLE `category`
 -- Indexes for table `ordered_products`
 --
 ALTER TABLE `ordered_products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ordered_products_ibfk_1` (`o_id`),
+  ADD KEY `ordered_products_ibfk_2` (`p_id`);
 
 --
 -- Indexes for table `order_details`
 --
 ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`o_id`);
+  ADD PRIMARY KEY (`o_id`),
+  ADD KEY `order_details_ibfk_1` (`u_id`);
 
 --
 -- Indexes for table `product_details`
 --
 ALTER TABLE `product_details`
   ADD PRIMARY KEY (`p_id`),
-  ADD KEY `c_id` (`c_id`,`b_id`),
-  ADD KEY `b_id` (`b_id`);
+  ADD KEY `product_details_ibfk_1` (`b_id`),
+  ADD KEY `product_details_ibfk_2` (`c_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`u_id`),
-  ADD UNIQUE KEY `email` (`email`) USING HASH;
+  ADD PRIMARY KEY (`u_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -287,31 +265,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin_details`
 --
 ALTER TABLE `admin_details`
-  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `b_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `b_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `cart_details`
 --
 ALTER TABLE `cart_details`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `c_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `c_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `ordered_products`
 --
 ALTER TABLE `ordered_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `order_details`
@@ -323,17 +301,36 @@ ALTER TABLE `order_details`
 -- AUTO_INCREMENT for table `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `cart_details`
+--
+ALTER TABLE `cart_details`
+  ADD CONSTRAINT `cart_details_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`),
+  ADD CONSTRAINT `cart_details_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product_details` (`p_id`);
+
+--
+-- Constraints for table `ordered_products`
+--
+ALTER TABLE `ordered_products`
+  ADD CONSTRAINT `ordered_products_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `product_details` (`p_id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `users` (`u_id`);
 
 --
 -- Constraints for table `product_details`
