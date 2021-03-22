e9a27846b2d2da837e5bf17b8c110ae2922d199b
Nikunj Thakor <38680557+nnikunjt@users.noreply.github.com>
2021-03-22 22:45:03 +0530

    fix size of user

--- contactus.php
+++ contactus.php
@@ -2,14 +2,12 @@
     include './top.php';
 ?>
  
-<div class=" mt-5 text-center ">
-    
-</div>
+
 <div class="card col-8 m-auto p-4 mt-5 ">
     
    <div class="row">
         <div class="col-md-5 col-xl-5 col-sm-11 pl-5 ">
-            <img src="./img/user-dummy-pic.png" class="img" alt="" srcset="">
+            <img src="./img/user-dummy-pic.png" class="contact-user-img" alt="" srcset="">
 
             <p><h4>NEHA SINGH.. </h4><br><b> CON NO-7228010920</b></p>
             <p>ACCOUNTS DEPARTMENT..<br>
@@ -21,10 +19,10 @@
             </dl>
             <p>GST NO-24AARPU7005J1ZA</p>
         </div>
-        <div class="col-md-6 col-xl-6 col-sm-11 ">
-            <h1 class="section-title">Contact Us </h1>
+        <div class="col-md-6 col-xl-6 col-sm-11 mt-n1">
+            <h1 class="section-title" style="margin-top:1.7rem;">Contact Us </h1>
 
-            <form action="" method="post" class="mt-5"> 
+            <form action="" method="post" class="mt-4"> 
             <div class="mb-3">
                 <label class="form-label">Name</label>
                 <input type="text" class="form-control" name="name" placeholder="Enter your name">
